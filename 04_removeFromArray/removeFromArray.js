3ac4f4f43399c9093e6b3f6204d3e1a0cf459542
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-02-16 20:33:25 +0530

    four done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,num) {
+    for(let i=0;i<arr.length;i++)
+        {
+            if(arr[i]==num)
+            {
+                 arr.splice(i,1);
+            }
+        }
+        return arr;    
 };
 
 // Do not edit below this line
